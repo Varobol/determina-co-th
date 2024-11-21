936582d2c315ad0cfe80ebab61ba66a503377573
taninpong <thaninpong@perfenterprise.com>
2024-11-21 13:08:24 +0700

    edit header

--- app/components/AppHeader.vue
+++ app/components/AppHeader.vue
@@ -3,72 +3,24 @@ const nuxtApp = useNuxtApp()
 const { activeHeadings, updateHeadings } = useScrollspy()
 
 const links = computed(() => [{
-  label: 'Features',
-  to: '#features',
+  label: 'Home',
+  to: '#home',
   icon: 'i-heroicons-cube-transparent',
-  active: activeHeadings.value.includes('features') && !activeHeadings.value.includes('pricing')
-}, {
-  label: 'Pricing',
-  to: '#pricing',
-  icon: 'i-heroicons-credit-card',
-  active: activeHeadings.value.includes('pricing') && !activeHeadings.value.includes('testimonials')
-}, {
-  label: 'Testimonials',
-  to: '#testimonials',
-  icon: 'i-heroicons-academic-cap',
-  active: activeHeadings.value.includes('testimonials')
-}, {
-  label: 'FAQ',
-  to: '#faq',
-  icon: 'i-heroicons-question-mark-circle',
-  active: activeHeadings.value.includes('faq')
+  active: activeHeadings.value.includes('home')
 }])
 
 nuxtApp.hooks.hookOnce('page:finish', () => {
   updateHeadings([
-    document.querySelector('#features'),
-    document.querySelector('#pricing'),
-    document.querySelector('#testimonials'),
-    document.querySelector('#faq')
+    document.querySelector('#home')
   ])
 })
 </script>
 
 <template>
-  <UHeader :links="links">
+  <UHeader>
     <template #logo>
-      Nuxt UI Pro <UBadge
-        label="Landing"
-        variant="subtle"
-        class="mb-0.5"
-      />
-    </template>
-
-    <template #right>
-      <UButton
-        label="Sign in"
-        color="white"
-        variant="ghost"
-        trailing-icon="i-heroicons-arrow-right-20-solid"
-        class="hidden lg:flex"
-      />
-    </template>
-
-    <template #panel>
-      <UAsideLinks :links="links" />
-
-      <UDivider class="my-6" />
-
-      <UButton
-        label="Sign in"
-        color="white"
-        block
-        class="mb-3"
-      />
-      <UButton
-        label="Get started"
-        block
-      />
+      <UAvatar size="sm" src="https://failfast.blob.core.windows.net/upload/webdetermina/DeterminaLogo.png" />
+      Determina
     </template>
   </UHeader>
 </template>
